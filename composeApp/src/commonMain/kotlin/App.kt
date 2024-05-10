2f1aeed99682ea62d96611cade45f6d6b2e0fcf2
nikitha2 <gullapalli.nikitha@gmail.com>
2024-05-09 17:56:15 -0700

    NextScreen Button

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -3,9 +3,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,34 +19,56 @@ import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import androidx.compose.material.Scaffold
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "Today's date is ${todaysDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
+        Scaffold(
+            bottomBar = {
+                PopulateNextScreenBtnContent(Modifier.fillMaxWidth().padding(horizontal = 24.dp)) {
+                    LocalTimeApplication()
+                }
+            }) {
+            var showContent by remember { mutableStateOf(false) }
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Today's date is ${todaysDate()}",
+                    modifier = Modifier.padding(20.dp),
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center
                 )
-            
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+                Button(onClick = { showContent = !showContent }) {
+                    Text("Click me!")
+                }
+                AnimatedVisibility(showContent) {
+                    val greeting = remember { Greeting().greet() }
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(painterResource(Res.drawable.compose_multiplatform), null)
+                        Text("Compose: $greeting")
+                    }
                 }
             }
         }
     }
 }
+
+@Composable
+fun PopulateNextScreenBtnContent(modifier: Modifier, onNextScreenBtnClickListener:@Composable () -> Unit = {}) {
+    var openNextScreen by remember { mutableStateOf(false) }
+
+    Button(modifier=modifier,
+           onClick = { openNextScreen = !openNextScreen}) {
+        Text("Next Screen")
+    }
+    AnimatedVisibility(openNextScreen) {
+        onNextScreenBtnClickListener.invoke()
+    }
+}
+
 
 fun todaysDate(): String {
     fun LocalDateTime.format() = toString().substringBefore('T')
---
+++ composeApp/src/commonMain/kotlin/LocalTimeApplication.kt
@@ -0,0 +1,27 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kmpdemo.composeapp.generated.resources.Res
+import kmpdemo.composeapp.generated.resources.compose_multiplatform
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import androidx.compose.material.Scaffold
+
+@Composable
+fun LocalTimeApplication() {
+    TODO("Not yet implemented")
+}
