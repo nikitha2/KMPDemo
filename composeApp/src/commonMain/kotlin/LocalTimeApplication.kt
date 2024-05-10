51e2f53b9d57dfbfb52fe28dceeaad4a06869b0d
nikitha2 <gullapalli.nikitha@gmail.com>
2024-05-09 17:56:27 -0700

    NextScreen Btn

--- composeApp/src/commonMain/kotlin/LocalTimeApplication.kt
+++ composeApp/src/commonMain/kotlin/LocalTimeApplication.kt
@@ -1,25 +1,4 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import kmpdemo.composeapp.generated.resources.Res
-import kmpdemo.composeapp.generated.resources.compose_multiplatform
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
 
 @Composable
 fun LocalTimeApplication() {
